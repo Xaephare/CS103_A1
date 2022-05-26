7cfa68453681df996f56d1bf47dfe31e75b06a0d
Louis Grace <louisgrace@onmail.com>
2022-05-26 18:04:19 +1200

    Working A & B inputs and outputs

--- Task_3/Task_3.cpp
+++ Task_3/Task_3.cpp
@@ -7,7 +7,7 @@ Q3.Write a program to add two 3x3 matrices called A and B and store the resultan
 into matrix C.Multiply the resultant matrix C by 3 and display the result.Assign the
 values to the A and B matrixes during the declaration.Display the output structure.
 
-Aand B arrays take user
+A and B arrays take user
 inputs as per the
 scenario.
 
@@ -22,10 +22,53 @@ The results have been
 displayed fully.
 */
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
+void arrInput(string arrId, int array[3][3]) {
+
+	int counter = 1;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			int x;
+			cout << "Enter value for array " << arrId << ", section " << counter << ": ";
+			cin >> x;
+			array[i][j] = x;
+			counter++;
+		}
+	}
+
+}
+
+void arrOutput(string arrId, int array[3][3]) {
+
+	cout << "\nArray " << arrId << ": ";
+	for (int i = 0; i < 3; i++) {
+		cout << "\n{";
+		for (int j = 0; j < 3; j++) {
+			cout << array[i][j];
+			if (j < 2) { cout << ", "; }
+		}
+		cout << "}";
+	}
+
+}
+
+int main() {
+
 	int A[3][3];
 	int B[3][3];
 	int C[3][3];
+
+
+	arrInput("A", A);
+	cout << "--------------------------------------" << endl;
+	arrInput("B", B);
+	cout << "--------------------------------------" << endl;
+
+	
+	arrOutput("A", A);
+	arrOutput("B", B);
+
+	return 0;
 }
