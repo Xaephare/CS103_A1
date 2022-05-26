5515c1e0ec507ec3492fe67a5849b239d9df7adc
Xaephare <louisgrace@onmail.com>
2022-05-26 14:54:39 +1200

    moving to the pc

---
+++ Task_3/Task_3.cpp
@@ -0,0 +1,31 @@
+//Louis Ulysse Grace - Section A - Question 3
+
+
+
+/*
+Q3.Write a program to add two 3x3 matrices called A and B and store the resultant
+into matrix C.Multiply the resultant matrix C by 3 and display the result.Assign the
+values to the A and B matrixes during the declaration.Display the output structure.
+
+Aand B arrays take user
+inputs as per the
+scenario.
+
+Added A and B stored
+into C matrix and
+displayed C matrix
+
+Multiplied C matrix by 3
+and displayed.
+
+The results have been
+displayed fully.
+*/
+#include <iostream>
+
+int main()
+{
+	int A[3][3];
+	int B[3][3];
+	int C[3][3];
+}
