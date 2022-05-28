bda1d7b19d14c38a63aadf666f7f72785beb7f27
Louis Grace <louisgrace@onmail.com>
2022-05-29 01:18:59 +1200

    Fully workng up to daily expenses

--- Task_6/Task_6.cpp
+++ Task_6/Task_6.cpp
@@ -4,44 +4,59 @@
 #include <vector>
 using namespace std;
 
-
+//Declare struct containing all necessary information.
 	struct Info {
-		struct date {
+		//structure within the main structure to contain the date.
+		struct Date {
 			int dd = 0, mm = 0, yyyy = 0;
-			//scanf("%d/%d/%d", &dd, &mm, &yyyy);
+			//scanf("%d/%d/%d", &dd, &mm, &yyyy) personal note to remember the scanf format.
 		} date;
 
-	int meal = 0;
-	int transport = 0;
-	int entertainment = 0;
-	int other = 0;
+	float meal = 0;
+	float transport = 0;
+	float entertainment = 0;
+	float other = 0;
 	};
 
+//Declare a vector that holds the struct(s).
 vector <Info> allExpenses;
 
+//Because why not, functions are fun. prints out the date of the date struct from the vector @ index#.
+void printDate(int index) {
+	cout << allExpenses[index].date.dd << "/" << allExpenses[index].date.mm << "/" << allExpenses[index].date.yyyy;
+}
 
-
+//Function to sort vector order by date, makes things easier comprehend.
+bool compareByDate(const Info &a, const Info &b) {
+	return true;
+}
 
 void addExpense() {
 	
-	bool adding = true;
 	Info newExpense;
 
-	while (adding) {
-		cout << "Enter date of expense separated by \"/\": ";
+	cout << "Enter date of expense separated by \"/\" (eg. dd/mm/yyyy): ";
+		
+	//Format scan just for fun/better readability.
+	scanf_s("%d/%d/%d", &newExpense.date.dd, &newExpense.date.mm, &newExpense.date.yyyy);
+
+	//Check for real dates.
+	while (newExpense.date.dd > 31 || newExpense.date.mm > 12 || newExpense.date.yyyy > 9999) {
+		cout << "Enter a real date, try again: ";
 		scanf_s("%d/%d/%d", &newExpense.date.dd, &newExpense.date.mm, &newExpense.date.yyyy);
-		cout << "Amount spent on meals: ";
-		cin >> newExpense.meal;
-		cout << "Amount spent on transport: ";
-		cin >> newExpense.transport;
-		cout << "Amount spent on entertainment: ";
-		cin >> newExpense.entertainment;
-		cout << "Amount spent on other categories: ";
-		cin >> newExpense.other;
-		//cout << newExpense.date.dd << "/" << newExpense.date.mm << "/" << newExpense.date.yyyy;
-		adding = false;
-		allExpenses.push_back(newExpense);
-	}
+		}
+
+	//Amounts spent.
+	cout << "Amount spent on meals: ";
+	cin >> newExpense.meal;
+	cout << "Amount spent on transport: ";
+	cin >> newExpense.transport;
+	cout << "Amount spent on entertainment: ";
+	cin >> newExpense.entertainment;
+	cout << "Amount spent in other categories: ";
+	cin >> newExpense.other;
+	//Add all the new structure values to the vector.
+	allExpenses.push_back(newExpense);
 }
 
 bool anotherExpense() {
@@ -88,40 +103,52 @@ int mainMenu() {
 	}
 }
 
-//void dailyReport() {}
+void dailyReport() {
 
-//void weeklyRepot() {}
+	int amount = allExpenses.size();
+	for (int i = 0; i < amount; i++) {
+		cout << "\nReport for date: "; 
+		printDate(i);
+		cout << "\n----------------------------------";
+		cout << "\nAmount spent on meals : $" << allExpenses[i].meal;
+		cout << "\nAmount spent on transport: $" << allExpenses[i].transport;
+		cout << "\nAmount spent on entertainment: $" << allExpenses[i].entertainment;
+		cout << "\nAmount spent in other categories: $" << allExpenses[i].other;
+		cout << "\nTotal Amount spent: $" << allExpenses[i].meal + allExpenses[i].transport + allExpenses[i].entertainment + allExpenses[i].other;
+		cout << endl << endl;
+	}
+	system("pause");
+}
+
+//void weeklyReport() {}
 
 
 
 int main() {
-
-	
-
 	
 	int choice;
-	bool another = true;
-	start:
-	
-	choice = mainMenu();
+	bool another;
 
+
+	start:
+	choice = mainMenu();
 	switch (choice) {
-	case 1: while (another) {
+	case 1: another = true;
+		while (another) {
 		addExpense();
 		another = anotherExpense();
 		}
 		break;
-	case 2: cout << "daily expenses";
+	case 2: dailyReport();
 		break;
 	case 3: cout << "weekly expenses";
 		break;
 	case 4:  return 0;
 	default: break;
- }
-
+	}
 	goto start;
 
-	cout << endl;
-	system("pause");
+
+
 	return 0;
 }
